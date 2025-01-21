8b5adf11fd273433efd4fd6fdd6a79defd53d279
mayankGupta <mayankguptasv01@gmail.com>
2025-01-21 13:22:42 +0530

    python

---
+++ OOP/03_basics.py
@@ -0,0 +1,11 @@
+# class book:
+#     def __init__(self,name):
+#         self.name = name
+    
+#     def __str__(self):
+#         return '10'
+#     def __repr__(self):
+#         return "i am  king"
+    
+# o = book('rajat')
+# print(o)
