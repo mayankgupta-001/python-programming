9b53e715432d94c6963c9731559cbfc389d9ad8c
mayankGupta <mayankguptasv01@gmail.com>
2025-02-02 11:51:33 +0530

    mro

--- OOP/MRO.py
+++ OOP/MRO.py
@@ -1,15 +1,43 @@
 #MRO: Method Resolution Order
 
-class A:
+# class A:
+#     def disp(self):
+#         print('class A')
+
+# class B(A):
+#     def disp(self):
+#         print('class B')
+#         super().disp()
+
+# ob = B()
+# ob.disp()
+# print(B.__mro__)
+
+
+
+
+class parent1:
+    def show(self):
+        print('parent class')
+
+class parent2:
+    def show(self):
+        print('parent class')
+
+class A(parent1):
     def show(self):
         print('class A')
+        super().show()
 
-class B:
+class B(parent2):
     def show(self):
         print('class B')
 
 class C(A,B):
     def show(self):
-        pass
+        print('class C')
+        super().show()
+
 ob = C()
 ob.show()
+print(C.__mro__)
